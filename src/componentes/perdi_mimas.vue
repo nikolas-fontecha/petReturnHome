7451ec9aaa5c001c6778218335bcfe07296c2765
nikolas-fontecha <49963424+nikolas-fontecha@users.noreply.github.com>
2020-04-15 12:49:51 -0500

    vistas con routers
    aqui estan las vistas usando routers

---
+++ src/componentes/perdi_mimas.vue
@@ -0,0 +1,32 @@
+<template>
+
+<div>
+    <center><h1 id='reg'> PERDI MI MASCOTA </h1></center>
+    
+</div>
+
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style scoped>
+
+#reg{
+    height: 40px;
+    width: 251px;
+    font-size: 37px;
+    background-color:blue;
+}
+
+#huella{
+    position: absolute;
+    width: 266px;
+    height: 241px;
+    left: 50px;
+    top: 117px;
+}
+</style>
