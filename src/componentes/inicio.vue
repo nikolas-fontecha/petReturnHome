503d69536bd4d0535aa20d94bc455fc5b775d65b
nicolas alejandro fontecha pardo <nafp_927@MacBook-Pro-de-nicolas.local>
2020-04-15 18:40:11 -0500

    cambio px

--- src/componentes/inicio.vue
+++ src/componentes/inicio.vue
@@ -6,7 +6,9 @@
     <input type="image" src="https://s3-alpha-sig.figma.com/img/554a/8b64/abb50fc6fb58226fb8d8fa2f53875ede?Expires=1587945600&Signature=U3N2cd5--vcZLZ2r9wPIZK94~Iw32nkf5ambkQ-0~6x5gAtkMgbkZQSzwGn9fAkg37PYQg14Pr~cfhM88mINvwQUR4Wec32kAXpBbiydrLo-P50zOM8H629GRkwfqrqXmqoU3Pc6wRZ3H4A47dFnosjEq2yggKr1zyaz04yUp4zyHR05e3S32wfe0LE8Vg92t6T-q42AYTeun4di0gmOrBZkwa8LJhcc5FQAACAfm8OA7SAUBzvGx849XfjI5KjC6ZMVb0yhJhTGF54CfqMMA4RHMvmFZctAFyoSOqxyS6xKj800zAebW4EVgFnAKX3MDR7vntOMHtfdOSZOzorgDw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" id="huella">
     <div><h10 id="NU">nombre usuario</h10></div>
     <input type="text" id="Nus">
+    <input type="text" id="contraseña">
     <router-link to="/registrar"><h5>registrar</h5></router-link>
+
     </center>
 
 </div>
@@ -70,11 +72,15 @@ export default {
         /* position: absolute; */
         width: 327px;
         height: 35px;
-        left: 24px;
+        left: 15px;
         top: 398px;
 
         background: #FFFFFF;
         border-radius: 4px;        
+    }
+
+    #contraseña{
+        /* position: absolute; */
     }
 
 </style>
