fd11279c0af5314f1bd71ed99baa850d5e1b6e3d
nikolas-fontecha <49963424+nikolas-fontecha@users.noreply.github.com>
2020-04-15 13:44:33 -0500

    cambie el titulo con un h2

---
+++ src/encontre_1mas.vue
@@ -0,0 +1,34 @@
+<template>
+
+<div>
+    <center>
+        <h2 id='reg'> REGISTRAR MASCOTA </h2>
+    </center>
+    
+</div>
+
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style scoped>
+
+#reg{
+    height: 40px;
+    width: 251px;
+    font-size: 37px;
+    background-color: brown;
+}
+
+#huella{
+    position: absolute;
+    width: 266px;
+    height: 241px;
+    left: 50px;
+    top: 117px;
+}
+</style>
